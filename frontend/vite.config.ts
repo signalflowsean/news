8be43f9f21b45f02747fcda574d3e1fce927014b
Sean Nealon <signalflowsean@gmail.com>
2026-02-28 16:01:39 -0500

    boiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
+// TODO: hash these files for cache busting
 export default defineConfig({
   plugins: [wasm(), topLevelAwait()],
   build: {
